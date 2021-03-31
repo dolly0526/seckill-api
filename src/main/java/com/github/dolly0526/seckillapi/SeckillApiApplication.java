5c57d1fb211ca7a1def52c6da5b59d86f5312a2e
Senyang Yu <dolly@mail.ustc.edu.cn>
2021-03-31 19:07:10 +0800

    初始化秒杀服务API

---
+++ src/main/java/com/github/dolly0526/seckillapi/SeckillApiApplication.java
@@ -0,0 +1,14 @@
+package com.github.dolly0526.seckillapi;
+
+import cn.hutool.extra.spring.EnableSpringUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableSpringUtil
+@SpringBootApplication
+public class SeckillApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillApiApplication.class, args);
+    }
+}
